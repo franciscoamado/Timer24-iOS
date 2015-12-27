e0a43d3820081c82a28f016fd17335128273eff8
Francisco Amado <franciscoamado@live.com.pt>
2015-12-27 19:40:01 +0000

    + ChameleonFramework pod + Initial designs

--- Timer24/ViewController.swift
+++ Timer24/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import ChameleonFramework
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var integerTimeLabel: UILabel!
+    @IBOutlet weak var bottomView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.setStatusBarStyle(UIStatusBarStyleContrast)
+        
+        view.backgroundColor = FlatOrange()
+        topView.backgroundColor = UIColor.clearColor()
+        bottomView.backgroundColor = UIColor.flatSandColor().colorWithAlphaComponent(0.5)
+        integerTimeLabel.textColor = ContrastColorOf(FlatOrange(), returnFlat:true)
+        startButton.titleLabel!.textColor = view.backgroundColor
+        startButton.setTitleColor(view.backgroundColor, forState:UIControlState.Normal)
+        startButton.setTitleColor(ComplementaryFlatColorOf(view.backgroundColor!), forState:UIControlState.Highlighted)
+        startButton.setTitleColor(ComplementaryFlatColorOf(view.backgroundColor!), forState:UIControlState.Selected)
+        
+        startButton.layer.cornerRadius = startButton.frame.width / 2
+        startButton.layer.borderWidth = 2
+        startButton.layer.borderColor = view.backgroundColor!.CGColor
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +42,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func startButtonAction(sender: UIButton) {
+//        startButton.layer.borderColor = sender.titleColorForState(UIControlState.Selected)?.CGColor;
+    }
 }
 
